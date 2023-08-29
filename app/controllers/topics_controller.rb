14d6f15eb576307822941d527a047887c7cd8a46
Yasu <raityou615@gmail.com>
2023-08-29 01:06:16 +0000

    2-6,2-7途中

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,8 +14,5 @@ class TopicsController < ApplicationController
   def update
     redirect_to'topics/edit'
   end
-  
-    get'/topics/new', to:'new' ,as'new_post'
-    get'/topics/edit', to:'edit', as'new_post'
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   
   # PostcController
-  get'posts/new', to:'posts#new', as'new_post'
+  get'posts/new', to:'posts#new', as:'new_post'
+  get'/topics/new', to:'topics#new' ,as:'new_topics'
+  get'/topics/edit', to:'topics#edit', as:'edit_topics'
   
 end
